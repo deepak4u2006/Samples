78f05d3ace1bd7cbefeb601411d83208c0d758d7
Vishwa Deepak <vishwadeepak@admins-MacBook-Pro.local>
2015-07-27 12:17:40 +0530

    First Commit Project Created

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Vishwa Deepak on 27/07/15.
+//  Copyright (c) 2015 learningiphonesdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Vishwa Deepak on 27/07/15.
+//  Copyright (c) 2015 learningiphonesdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
